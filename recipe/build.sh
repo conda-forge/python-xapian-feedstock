1bb0b79dd2a36336a26af529a8b9c93f54eed4db
Holger Kohr <ho.kohr@zoho.com>
2020-02-27 23:30:13 +0100

    Fix pinning, add patch for xapian-config option

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,19 @@
-set -xu
+set -xe
 
 if [[ ${PY3K} == "1" ]]; then
     PYFLAG="--with-python3"
     PYTHON2=
-    PYTHON3=$PYTHON
+    PYTHON3=${PYTHON}
 else
     PYFLAG="--with-python"
-    PYTHON2=$PYTHON
+    PYTHON2=${PYTHON}
     PYTHON3=
 fi
 
-$PREFIX/bin/xapian-config --version
-$PREFIX/bin/xapian-config --cxxflags
-$PREFIX/bin/xapian-config --libs
-$PREFIX/bin/xapian-config --ltlibs
-
-PYTHON2=$PYTHON2 PYTHON3=$PYTHON3 \
-LDFLAGS=$LDFLAGS \
+PYTHON2=${PYTHON2} PYTHON3=${PYTHON3} \
+LDFLAGS=${LDFLAGS} \
 ./configure \
-    CPPFLAGS=-I$PREFIX/include \
+    CPPFLAGS=-I${PREFIX}/include \
     --without-php \
     --without-php7 \
     --without-ruby \
@@ -29,8 +24,7 @@ LDFLAGS=$LDFLAGS \
     --without-lua \
     --disable-documentation \
     $PYFLAG \
-    --prefix=$PREFIX
+    --prefix=${PREFIX}
 
-make && make install
-rm -rf $PREFIX/share/doc/xapian-bindings
-
+make
+make install prefix=${PREFIX}
