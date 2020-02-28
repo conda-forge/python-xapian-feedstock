d7dcd93d0c4628968543a3764dda7cf60ee21ddf
Holger Kohr <ho.kohr@zoho.com>
2020-02-28 20:05:50 +0100

    Add pythontest.py to package test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,7 +2,7 @@ set -xue
 
 PYTHON_VERSION_MAJOR=$(${PYTHON} -c "import sys; print(sys.version_info.major)")
 
-# Some of the smoketests fail on Py2k, so for now we only run them on Py3k
+# Some of the smoketests currently fail on Py2k, so we only run them on Py3k
 if [[ ${PYTHON_VERSION_MAJOR} == "2" ]]; then
   EXAMPLES_DIR="${PREFIX}/share/doc/xapian-bindings/python/examples"
   RUN_TESTS=0
@@ -16,4 +16,5 @@ fi
 
 if [[ ${RUN_TESTS} == 1 ]]; then
   VERBOSE=1 ${PYTHON} "${EXAMPLES_DIR}/test/smoketest.py"
+  VERBOSE=1 ${PYTHON} "${EXAMPLES_DIR}/test/pythontest.py"
 fi
